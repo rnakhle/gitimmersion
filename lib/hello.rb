88a53600db271ee4cc203d1bf20c9fd7a92158d7
Royce Nakhle <rnakhle@uncc.edu>
2017-11-11 18:42:40 +0000

    Added a Rakefile.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 # Default is World
 # Author: Royce Nakhle (rnakhle@uncc.edu)
-=======
-# This is an unwanted but committed comment
-
->>>>>>> 3409e7fc28119b5fb6e9433d6b04ef5a48fd3c72
 name = ARGV.first || "World"
-
 puts "Hello, #{name}!"
