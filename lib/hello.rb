488de2ce83dc2e326a852a4fc36450f37950a388
Royce Nakhle <rnakhle@uncc.edu>
2017-11-11 19:11:54 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,4 @@
-# Default is World
-# Author: Royce Nakhle (rnakhle@uncc.edu)
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
